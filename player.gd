24adae31eb535edd944d136f6b25d28e25039fc9
Tanish Kushalkar <tanishkushalkar@gmail.com>
2024-03-01 10:18:22 +0530

    Add ship animation movement based on key

--- player.gd
+++ player.gd
@@ -10,5 +10,14 @@ func _ready():
 func _process(delta):
 	#left right up down
 	var input = Input.get_vector("left","right","up","down")
+	if input.x > 0:
+		$Ship.frame = 2
+		$Ship/Boosters.animation = "right"
+	elif input.x < 0:
+		$Ship.frame = 0
+		$Ship/Boosters.animation = "left"
+	else:
+		$Ship.frame = 1
+		$Ship/Boosters.animation = "forward"
 	position += input * speed * delta
 	position = position.clamp(Vector2(8, 8), screensize - Vector2(8, 8))
